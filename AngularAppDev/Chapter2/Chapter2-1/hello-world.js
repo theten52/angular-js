4627060b7d7a24acd8c885460b26f0c883bc129a
wangj748 <jin.wang.-nd@disney.com>
2022-07-07 19:05:35 +0800

    add new code

--- AngularAppDev/Chapter2/Chapter2-1/hello-world.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.helloWorld = void 0;
-function helloWorld() {
-    console.log('hello, world');
-}
-exports.helloWorld = helloWorld;
-// helloWorld();// 添加对函数helloWorld()的调用
--- AngularAppDev/Chapter2/Chapter2-1/index.ts
+++ AngularAppDev/Chapter2/Chapter2-1/index.ts
@@ -1,5 +1,6 @@
 // 从当前目录下导入模块hello-world(.ts)内的公共函数helloWorld
-import { helloWorld } from './hello-world'; 
+import { helloWorld } from './hello-world';
 
 debugger; // 断点标识符
 helloWorld(); // 调用helloWorld()函数
+console.log("debug end");
