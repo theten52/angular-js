55969abd41c28abe214461468330ee6fd5f05cfb
wangj748 <Jin.Wang.-ND@disney.com>
2022-08-20 10:36:25 +0800

    ano

---
+++ AngularAppDev/Chapter4/003-decorators/002-metadatas/003-index.ts
@@ -0,0 +1,15 @@
+// 导入本节开头的人类People
+import { People } from '../001-introduction/001-introduction';
+// 导入本小节的泛型对象工厂类ObjectFactory
+import { ObjectFactory } from './002-object-factory';
+// 导入元数据定义函数
+import { setPeopleMetadata, setAddressMetadata } from './001-utility';
+
+// 定义元数据
+setPeopleMetadata();
+setAddressMetadata();
+
+// 创建人类对象工厂，并将JSON转换成包含address属性的人类对象
+let peopleFactory = new ObjectFactory(People);
+let person = peopleFactory.createObject('{"name": "Lcng", "age": 1, "address": {"street": "Baldwin Street", "houseNumber": 3}}');
+person.greet();
