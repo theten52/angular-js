4f54c9415a5d91c1ba94bc6afdb5f684df9606e6
wangj748 <Jin.Wang.-ND@disney.com>
2022-08-20 23:30:49 +0800

    ano

--- AngularAppDev/Chapter5/declaration-files/ambient-module/index.ts
+++ AngularAppDev/Chapter5/declaration-files/ambient-module/index.ts
@@ -1,9 +1,9 @@
-/// <reference path="../../node_modules/hero-package/heroes.d.ts" />
-import { Superman } from 'hero-package/dc';
-import { Ironman } from 'hero-package/marvel';
+// /// <reference path="../../node_modules/hero-package/heroes.d.ts" />
+// import { Superman } from 'hero-package/dc';
+// import { Ironman } from 'hero-package/marvel';
 
-let superman = new Superman();
-console.log(superman.name);
+// let superman = new Superman();
+// console.log(superman.name);
 
-let ironman = new Ironman();
-console.log(ironman.name);
+// let ironman = new Ironman();
+// console.log(ironman.name);
