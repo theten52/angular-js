28dff5f46a1358ce46ae68166cbf6f3031fa1a72
wangj748 <Jin.Wang.-ND@disney.com>
2022-08-24 12:24:34 +0800

    change import to es mode

--- web-development-with-node-and-express/ch04/meadowlark.js
+++ web-development-with-node-and-express/ch04/meadowlark.js
@@ -1,39 +1,48 @@
-var express = require('express');
-var fortune = require('./lib/fortune.js');
+import express, {
+  static as e_static
+} from 'express';
+import {
+  getFortune
+} from './lib/fortune.js';
 
 var app = express();
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
-	.create({ defaultLayout:'main' });
+  .create({
+    defaultLayout: 'main'
+  });
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(__dirname + '/public'));
+app.use(e_static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	res.render('home');
+app.get('/', function (req, res) {
+  res.render('home');
 });
-app.get('/about', function(req,res){
-	res.render('about', { fortune: fortune.getFortune() } );
+app.get('/about', function (req, res) {
+  res.render('about', {
+    fortune: getFortune()
+  });
 });
 
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
-	res.status(404);
-	res.render('404');
+app.use(function (req, res, next) {
+  res.status(404);
+  res.render('404');
 });
 
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.status(500);
-	res.render('500');
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' + 
-    app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.');
 });
