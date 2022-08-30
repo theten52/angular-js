4b5d81d350926f2aa7c94b45dc4937951809b438
wangj748 <Jin.Wang.-ND@disney.com>
2022-08-30 11:10:26 +0800

    add X-Forwarded-For to fetch xiapi

--- node-quick-starter/fetch.js
+++ node-quick-starter/fetch.js
@@ -1,11 +1,8 @@
 const fetch = require('node-fetch');
 
-
-
 var myHeaders = {
   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 YaBrowser/22.7.3.796 Yowser/2.5 Safari/537.36",
-  // "Cookie": "REC_T_ID=ab5faaf2-2796-11ed-a7df-2cea7fac566d; SPC_F=K5j9Z9wgZy8gofQABu2ZEJQJuYsnObTE; SPC_R_T_ID=nrn0wm0HYLzGvgOhcoVyEZMQvyiyqhJYPGVtY9MHfa9NB+UyCJbJhV48jZATbFjX6kAhCWx72DE0VCXQD7bAEiOiq9AUc4vOQ5eWRNtLDBWtY8ruRH0nPBs8Mtljn9vTmaJxEqzD02HQN8iKjPLTx5Qq+V8Ud+Z4q1cBnfuMoFA=; SPC_R_T_IV=MU9lSDFRWVZsRVRkN0ZCaA==; SPC_SI=QvsFYwAAAABlQ0N1aTJmbZfsQQAAAAAAWGg1SDhpc0s=; SPC_T_ID=nrn0wm0HYLzGvgOhcoVyEZMQvyiyqhJYPGVtY9MHfa9NB+UyCJbJhV48jZATbFjX6kAhCWx72DE0VCXQD7bAEiOiq9AUc4vOQ5eWRNtLDBWtY8ruRH0nPBs8Mtljn9vTmaJxEqzD02HQN8iKjPLTx5Qq+V8Ud+Z4q1cBnfuMoFA=; SPC_T_IV=MU9lSDFRWVZsRVRkN0ZCaA==",
-
+  "X-Forwarded-For": "10.139.253.34"
 }
 var requestOptions = {
   method: 'GET',
