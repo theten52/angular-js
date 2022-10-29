c7bcc3eeafd6389514e494a2b52e80fa83c606f5
JWang <1018863434@qq.com>
2022-10-29 13:05:37 +0800

    add math module

--- 009-requirejs/scripts/require.js
+++ 009-requirejs/scripts/require.js
@@ -1,7 +1,11 @@
-requirejs(["helper/util"], function (util) {
-    //此函数在加载scripts/helper/util.js时调用。
-    //如果util.js调用define()，则直到
-    //util的依赖项已加载，util参数将保持
-    //“helper/util"的模块值。
-    console.log("require js 已经加载");
-});
+requirejs(
+    ["helper/util", "helper/math"],
+    function (util, math) {
+        //此函数在加载scripts/helper/util.js时调用。
+        //如果util.js调用define()，则直到
+        //util的依赖项已加载，util参数将保持
+        //“helper/util"的模块值。
+        console.log("require js 已经加载");
+        console.log("1+1=", math.add(1, 1))
+    }
+);
